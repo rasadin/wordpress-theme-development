8b0b0a63f44c7d28e94a4e3c5103a4d0f918d215
Mohd. Rasadin <12105708+rasadin@users.noreply.github.com>
2022-11-04 21:36:17 +0600

    Add files via upload

---
+++ theme with grunt/updated/webalive/inc/customizer/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Webalive Theme Customizer
+ *
+ * @package webalive
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function webalive_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'webalive_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'webalive_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'webalive_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function webalive_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function webalive_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function webalive_customize_preview_js() {
+	wp_enqueue_script( 'webalive-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'webalive_customize_preview_js' );
